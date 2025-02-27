b47cc30b26670bcce27d6c059cf907c682738534
lorenzosalti <lorenzosalti@gmail.com>
2025-02-27 18:18:30 +0100

    feat: dadi complete

--- js-dadi/js/app.js
+++ js-dadi/js/app.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+// Dichiarazione variabili con numero randomico da 1 a 6
+let userThrow = Math.ceil(Math.random() * 6);
+let pcThrow = Math.ceil(Math.random() * 6);
+
+// Stampa dei due risulatati
+console.log(`Il lancio dell'utente ha dato risultato: ${userThrow}`);
+console.log(`Il lancio del computer ha dato risultato: ${pcThrow}`);
+
+// Determinazione del vincitore 
+if (userThrow > pcThrow) console.log("Vince l'utente!");
+else if (pcThrow > userThrow) console.log("Vince il computer!");
+else if (pcThrow = userThrow) console.log("Paritá!");
